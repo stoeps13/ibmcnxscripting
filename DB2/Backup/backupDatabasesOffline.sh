2b1151c61c6e64cddd731702271903988b996f54
Christoph Stoettner <christoph.stoettner@stoeps.de>
2013-09-15 15:55:29 +0200

    Added variables for better configuration to DB2 Backup scripts

--- DB2/Backup/backupDatabasesOffline.sh
+++ DB2/Backup/backupDatabasesOffline.sh
@@ -3,11 +3,15 @@
 # Author: Christoph Stoettner
 # E-Mail: christoph.stoettner@stoeps.de
 
+# Set Backup-Directory, change to your environment
+export DBBACKUPPATH=/opt/db2backup
+# get all databases of db2 instance
 databases=$(db2 list database directory | grep alias | awk '{print $4}' | sort)
 
+# Loop through list of databases:
 for database in ${databases[@]}
 do
  echo $database
- db2 backup database $database to /opt/db2backup COMPRESS
+db2 backup database $database to $DBBACKUPPATH COMPRESS
 done
 
