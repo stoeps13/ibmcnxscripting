b2d8162b3ec30af91a7ac1d4780b70eb84f7d012
Christoph Stoettner <christoph.stoettner@stoeps.de>
2013-12-02 22:20:44 +0100

    some comments on line end

--- DB2/reorg.sh
+++ DB2/reorg.sh
@@ -10,10 +10,11 @@
 # Author: Christoph Stoettner
 # E-Mail: christoph.stoettner@stoeps.de
 #
+# line end with @
 for db in activities blogs cognos communities dogear files forum libraries.gcd libraries.os metrics mobile wikis ; do
 	db2 -td@ -vf $db/db2/reorg.sql
 done
+# homepage and profiles use ; for line end
 for db in homepage profiles ; do
 	db2 -tvf $db/db2/reorg.sql
 done
-
