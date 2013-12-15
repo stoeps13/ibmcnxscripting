269535917aa80382e16ac46e5ebc309b947bcc80
Christoph Stoettner <christoph.stoettner@stoeps.de>
2013-12-15 14:04:59 +0100

    initial version, no func implemented

--- WebSphere/cfgLtpa.py
+++ WebSphere/cfgLtpa.py
@@ -5,6 +5,7 @@
 # Idea, setting Domain, TimeOut and Type (1 or 2) through this script
 # get|setLTPATimeout
 # AdminTask.setLTPATimeout('-timeout 120')
+# AdminTask.setLTPATimeout(['-timeout', '120'])
 # AdminTask.getLTPATimeout(['-securityDomainName', 'testDomain'])
 # AdminTask.configureLTPAToken
 
