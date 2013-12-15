5e923610fd158a64694d67baae9162374dc5652f
Christoph Stoettner <christoph.stoettner@stoeps.de>
2013-12-15 18:11:43 +0100

    Tested, working for Linux, Windows testing todo

--- WebSphere/cfgLogFiles.py
+++ WebSphere/cfgLogFiles.py
@@ -6,25 +6,35 @@
 #
 # test if nodeagent and dmgr are in servers-list
 
-maxLogSize = int( raw_input( '\tMax Logfile size in MB (int): ' ) )
-maxLogHistory = int( raw_input( '\tMax Number of Backup Files (int): ' ) )
+maxLogSize = int( raw_input( '\tMax Logfile size in MB (1-50): ' ) )
+maxLogHistory = int( raw_input( '\tMax Number of Backup Files (1-200): ' ) )
 
 print "\nChanging the SystemOut & SystemErr log file rotation settings\n"
 
-# Get a list of all servers in WAS cell
-servers = AdminTask.listServers( '[-serverType APPLICATION_SERVER]' ).splitlines()
+# Get a list of all servers in WAS cell (dmgr, nodeagents, AppServer, webserver)
+servers = AdminTask.listServers().splitlines()
+# Get a list of all webservers
+webservers = AdminTask.listServers( '[-serverType WEB_SERVER]' ).splitlines()
+
+# Remove webserver from servers list
+for webserver in webservers:
+    servers.remove( webserver )
 
 for server in servers:
-    # get ID of server
-    serverID = AdminConfig.getid( server )
+    # Print Node- and Servername
+    print "Change log setting for: " + server.split( '(' )[1].split( '/' )[3] + ' ' + server.split( '(' )[1].split( '/' )[5].split( '|' )[0]
+    try:
 
-    # output and errorStream
-    systemOut = AdminConfig.showAttribute( serverID, 'outputStreamRedirect' )
-    systemErr = AdminConfig.showAttribute( serverID, 'errorStreamRedirect' )
+        # output and errorStream
+        systemOut = AdminConfig.showAttribute( server, 'outputStreamRedirect' )
+        systemErr = AdminConfig.showAttribute( server, 'errorStreamRedirect' )
 
-    # modify settings for log Size and History
-    AdminConfig.modify( systemOut, '[[rolloverSize ' + maxLogSize + '] [maxNumberOfBackupFiles ' + maxLogHistory + ']]' )
-    AdminConfig.modify( systemErr, '[[rolloverSize ' + maxLogSize + '] [maxNumberOfBackupFiles ' + maxLogHistory + ']]' )
+        # modify settings for log Size and History
+        AdminConfig.modify( systemOut, '[[rolloverSize ' + str( maxLogSize ) + '] [maxNumberOfBackupFiles ' + str( maxLogHistory ) + ']]' )
+        AdminConfig.modify( systemErr, '[[rolloverSize ' + str( maxLogSize ) + '] [maxNumberOfBackupFiles ' + str( maxLogHistory ) + ']]' )
+
+    except:
+        print "Error on setting Log File Size"
 
 # Save Configuration
 AdminConfig.save()
