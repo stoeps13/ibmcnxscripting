835376bad8e83670d226d4b5ae67960ce50d5930
Christoph Stoettner <christoph.stoettner@stoeps.de>
2013-12-15 21:13:20 +0100

    Added time based rollover

--- WebSphere/cfgLogFiles.py
+++ WebSphere/cfgLogFiles.py
@@ -2,14 +2,12 @@
 # Author: Christoph Stoettner
 # E-Mail: christoph.stoettner@stoeps.de
 #
-# ToDo:
-#
-# test if nodeagent and dmgr are in servers-list
-
-maxLogSize = int( raw_input( '\tMax Logfile size in MB (1-50): ' ) )
-maxLogHistory = int( raw_input( '\tMax Number of Backup Files (1-200): ' ) )
 
 print "\nChanging the SystemOut & SystemErr log file rotation settings\n"
+
+rollOverType = raw_input( '\tRolloverType (SIZE, BOTH): ' )
+maxLogSize = int( raw_input( '\tMax Logfile size in MB (1-50): ' ) )
+maxLogHistory = int( raw_input( '\tMax Number of Backup Files (1-200): ' ) )
 
 # Get a list of all servers in WAS cell (dmgr, nodeagents, AppServer, webserver)
 servers = AdminTask.listServers().splitlines()
@@ -20,22 +18,41 @@ webservers = AdminTask.listServers( '[-serverType WEB_SERVER]' ).splitlines()
 for webserver in webservers:
     servers.remove( webserver )
 
+print ''
+
 for server in servers:
-    # Print Node- and Servername
-    print "Change log setting for: " + server.split( '(' )[1].split( '/' )[3] + ' ' + server.split( '(' )[1].split( '/' )[5].split( '|' )[0]
     try:
+
+        nodename = server.split( '(' )[1].split( '/' )[3]
+        servername = server.split( '(' )[1].split( '/' )[5].split( '|' )[0]
+
+        if len( nodename ) < 10:
+            tab = '\t\t\t'
+        elif len( nodename ) > 10 and len( nodename ) < 15:
+            tab = '\t\t'
+        else:
+            tab = '\t'
+
+        # Print Node- and Servername
+        print "\tChange log setting for: " + nodename + tab + servername
 
         # output and errorStream
         systemOut = AdminConfig.showAttribute( server, 'outputStreamRedirect' )
         systemErr = AdminConfig.showAttribute( server, 'errorStreamRedirect' )
 
+        if rollOverType == 'BOTH' or rollOverType.upper() == 'B':
+            logSetting = '[[rolloverSize ' + str( maxLogSize ) + '] [rolloverPeriod 24] [maxNumberOfBackupFiles ' + str( maxLogHistory ) + ']]'
+        else:
+            logSetting = '[[rolloverSize ' + str( maxLogSize ) + '] [maxNumberOfBackupFiles ' + str( maxLogHistory ) + ']]'
+
         # modify settings for log Size and History
-        AdminConfig.modify( systemOut, '[[rolloverSize ' + str( maxLogSize ) + '] [maxNumberOfBackupFiles ' + str( maxLogHistory ) + ']]' )
-        AdminConfig.modify( systemErr, '[[rolloverSize ' + str( maxLogSize ) + '] [maxNumberOfBackupFiles ' + str( maxLogHistory ) + ']]' )
+        AdminConfig.modify( systemOut, logSetting )
+        AdminConfig.modify( systemErr, logSetting )
 
     except:
         print "Error on setting Log File Size"
 
 # Save Configuration
 AdminConfig.save()
+print '\n\n'
 
