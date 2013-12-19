e7ebb1627d2a535a0fe3f121d62fb147874e4d16
Christoph Stoettner <christoph.stoettner@stoeps.de>
2013-12-19 11:13:04 +0100

    MemberService Scripts, rewritten with sql connect and raw_input

---
+++ WebSphere/cnxCheckExIDByEmail.py
@@ -0,0 +1,92 @@
+import os
+import sys
+from java.util import Properties
+
+# add the jar to your classpath, then import it
+# better to read WebSphere variable PROFILES_JDBC_DRIVER_HOME
+sys.path.append( '/opt/IBM/JDBC/db2jcc4.jar' )
+import com.ibm.db2.jcc.DB2Driver as Driver
+
+# Change User and Password
+props = Properties()
+props.put( 'user', 'lcuser' )
+props.put( 'password', 'password' )
+
+# Change Hostname, Port and maybe DB Name
+conn = Driver().connect( 'jdbc:db2://cnxdb2.stoeps.local:50000/PEOPLEDB', props )
+
+stmt = conn.createStatement()
+
+email = raw_input( "Mail address of profile you want to check: " ).lower()
+
+sql = 'select PROF_UID_LOWER,PROF_MAIL_LOWER,PROF_GUID,PROF_MAIL from empinst.employee where PROF_MAIL_LOWER = \'' + email + '\' order by PROF_UID_LOWER'
+rs = stmt.executeQuery( sql )
+
+employeeList = []
+while ( rs.next() ):
+    row = {}
+    row['PROF_UID_LOWER'] = rs.getString( 1 )
+    row['PROF_MAIL_LOWER'] = rs.getString( 2 )
+    row['PROF_GUID'] = rs.getString( 3 )
+    row['PROF_MAIL'] = rs.getString( 4 )
+    employeeList.append( row )
+
+rs.close()
+stmt.close()
+conn.close()
+
+# print the result
+for e in employeeList:
+    # print e['PROF_UID_LOWER'] + "\t\t" + e['PROF_MAIL_LOWER'] + "\t\t" + e['PROF_GUID']
+    # print e['PROF_MAIL']
+    print "Profiles:\t\t\t" + e['PROF_GUID']
+
+LOGIN = e['PROF_MAIL']
+try:
+   print "Activities:\t\t\t",
+   ActivitiesMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Blogs:\t\t\t\t",
+   BlogsMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Communities:\t\t\t",
+   CommunitiesMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Dogear:\t\t\t\t",
+   DogearMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Files:\t\t\t\t",
+   FilesMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Forums:\t\t\t\t",
+   ForumsMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "News, Search, Homepage:\t\t",
+   NewsMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
+try:
+   print "Wikis:\t\t\t\t",
+   WikisMemberService.getMemberExtIdByLogin( LOGIN )
+except:
+   print 'No user with Login ' + LOGIN + ' found'
+
