0b4ba8cc979c616634c51145bbf9638f1ce3f3aa
kbild <klaus.bild@gmail.com>
2014-01-07 11:42:07 +0100

    Create backupDatabasesOnline.bat

---
+++ DB2/Backup/backupDatabasesOnline.bat
@@ -0,0 +1,16 @@
+@echo off 
+REM
+REM Author: Klaus Bild
+REM E-Mail: klaus.bild@gmail.com
+
+REM Needed for variable expansion
+SETLOCAL ENABLEDELAYEDEXPANSION
+REM Set Backup-Directory, change to your environment
+set DBBACKUPPATH=E:\Transfer
+REM get all databases of db2 instance and loop through the list
+FOR /F "tokens=5" %%a IN ('DB2CMD.EXE -c -w -i DB2 list DATABASE DIRECTORY ^| findstr "Alias"') DO (
+				set DATABASES=%%a,!DATABASES!
+				title Starting database %%a backup on %date% at %time%... 
+				DB2CMD.EXE -c -w -i DB2 BACKUP DATABASE %%a ONLINE to %DBBACKUPPATH% COMPRESS INCLUDE LOGS
+				)
+ECHO Successfull created backups for %DATABASES% in %DBBACKUPPATH%
