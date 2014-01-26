9b06666937d106c667801ac0b81c7221d62c4e91
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-01-25 23:52:47 -0500

    Removed a test class

--- WebSphere/ibmcnxscript.py
+++ WebSphere/ibmcnxscript.py
@@ -85,27 +85,3 @@ def showJdbcProviders():
         print "Class Name: \t\t" + providerImplementationClassName
         print  "\n"
 
-
-def importJar(jarFile):
-    '''
-    import a jar at runtime (needed for JDBC [Class.forName])
-
-    adapted from http://forum.java.sun.com/thread.jspa?threadID=300557
-    Author: SG Langer Jan 2007 translated the above Java to Jython
-    Author: seansummers@gmail.com simplified and updated for jython-2.5.3b3
-
-    >>> importJar('jars/jtds-1.2.5.jar')
-    >>> import java.lang.Class
-    >>> java.lang.Class.forName('net.sourceforge.jtds.jdbc.Driver')
-    <type 'net.sourceforge.jtds.jdbc.Driver'>
-    '''
-    from java.net import URL, URLClassLoader
-    from java.lang import ClassLoader
-    from java.io import File
-    m = URLClassLoader.getDeclaredMethod("addURL", [URL])
-    m.accessible = 1
-    m.invoke(ClassLoader.getSystemClassLoader(), [File(jarFile).toURL()])
-
-    if __name__ == '__main__':
-        import doctest
-        doctest.testmod()
