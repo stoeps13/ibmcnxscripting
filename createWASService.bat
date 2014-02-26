247e8a1977fab32271536b19718bc211be28038e
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-02-26 20:38:56 +0100

    Changes for providing a run as user for service

--- createWASService.bat
+++ createWASService.bat
@@ -1,3 +1,3 @@
 cd %WAS_HOME%/bin
-wasservice.bat -add Dmgr -serverName dmgr -profilePath D:\IBM\WebSphere\AppServer\profiles\Dmgr01 -encodeParams -restart true -startType automatic -stopArgs “-username wasadmin -password password”
-wasservice.bat -add Node -serverName nodeagent -profilePath D:\IBM\WebSphere\AppServer\profiles\AppSrv01 -encodeParams -restart true -startType automatic -stopArgs '-username wasadmin -password password -stopservers'
+wasservice.exe -add Dmgr -serverName dmgr -profilePath D:\IBM\WebSphere\AppServer\profiles\Dmgr01 -userid serviceaccount - password password  -encodeParams -restart true -startType automatic -stopArgs "-username wasadmin -password password"
+wasservice.exe -add Node -serverName nodeagent -profilePath D:\IBM\WebSphere\AppServer\profiles\AppSrv01 -userid serviceaccount - password password -encodeParams -restart true -startType automatic -stopArgs "-username wasadmin -password password -stopservers"
