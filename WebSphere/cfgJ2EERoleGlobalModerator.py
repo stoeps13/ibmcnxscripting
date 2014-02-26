ee1a9e39c863cb37f09a3e28c8502574b55a4610
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-02-26 20:37:48 +0100

    Initial versions, changing j2ee roles for special functions (social mail, metrics, moderation)

---
+++ WebSphere/cfgJ2EERoleGlobalModerator.py
@@ -0,0 +1,62 @@
+# cfgJ2EERoleGlobalModerator
+#
+# Author: Christoph Stoettner
+# Blog: http://www.stoeps.de
+# E-Mail: nospam@stoeps.de
+#
+# Description:
+# Script is tested with IBM Connections 4.5 CR2, CR3
+#
+# You can enable and disable Global Moderation through J2EE Roles with this script
+
+# History:
+# 20140225  Christoph Stoettner     Initial Version
+
+# import ibmcnxscript
+
+def setJ2EERole(appName,role_auth,role_users,role_groups):
+    AdminApp.edit( appName, '[-MapRolesToUsers [["global-moderator" ' + role_auth + ' "' + role_users + '" "' + role_groups + '" ]]]' )
+        
+def printMenu():
+    state = ''
+    while state != ( 'ENABLE' or 'DISABLE' or 'EXIT' ):
+        state = raw_input( 'Do you want to enable or disable Moderation? (E|D|X)(ENABLE|DISABLE|EXIT)' ).upper()
+        if state == 'E':
+            state = 'ENABLE'
+            break
+        elif state == 'D':
+            state = 'DISABLE'
+            break
+        elif state == 'X':
+            state = 'EXIT'
+            break
+        else:
+            continue
+            
+    if state == 'ENABLE':
+        print 'Enable Global Moderation'
+        auth = ''
+        while auth != ( 'Y' or 'YES' or 'N' or 'NO'):
+            auth = raw_input('Enable Global-Moderator Role for \"All-Authenticated in Realm\"? (Y|N)(YES|NO)').upper()
+            if auth == 'Y':
+                role_auth = 'No Yes'
+            else:
+                role_auth = 'No No'
+        role_users = raw_input('Enable Global-Moderator Role for single Users? (Type casesensitiv uid, empty for none, multiple uids seperate by \"|\")')
+        role_groups = raw_input('Enable Global-Moderator Role for a Group? (Type casesensitiv Groupname, empty for no groups, multiple Groups seperated by \"|\")')
+             
+    elif state == 'DISABLE':
+        print 'Disable Global Moderation'   
+        role_auth='No No'
+        role_users=''
+        role_groups=''
+        
+    apps = ['Blogs','Common','Communities','Files','Forums','Moderation','WidgetContainer']    
+    for app in apps:     
+        print "Setting Role for " + app
+        setJ2EERole(app,role_auth,role_users,role_groups)
+    
+    AdminConfig.save()       
+    
+
+printMenu()
---
+++ WebSphere/cfgJ2EERoleSocialMail.py
@@ -0,0 +1,62 @@
+# cfgJ2EERolesSocialMail
+#
+# Author: Christoph Stoettner
+# Blog: http://www.stoeps.de
+# E-Mail: nospam@stoeps.de
+#
+# Description:
+# Script is tested with IBM Connections 4.5 CR2, CR3
+#
+# You can enable and disable Socialmail Integration through J2EE Roles with this script
+
+# History:
+# 20140225  Christoph Stoettner     Initial Version
+
+# import ibmcnxscript
+
+def setJ2EERole(appName,role_auth,role_users,role_groups):
+    AdminApp.edit( appName, '[-MapRolesToUsers [["mail-user" ' + role_auth + ' "' + role_users + '" "' + role_groups + '" ]]]' )
+        
+def printMenu():
+    state = ''
+    while state != ( 'ENABLE' or 'DISABLE' or 'EXIT' ):
+        state = raw_input( 'Do you want to enable or disable Socialmail Integration? (E|D|X)(ENABLE|DISABLE|EXIT)' ).upper()
+        if state == 'E':
+            state = 'ENABLE'
+            break
+        elif state == 'D':
+            state = 'DISABLE'
+            break
+        elif state == 'X':
+            state = 'EXIT'
+            break
+        else:
+            continue
+            
+    if state == 'ENABLE':
+        print 'Enable socialmail'
+        auth = ''
+        while auth != ( 'Y' or 'YES' or 'N' or 'NO'):
+            auth = raw_input('Enable Mail-User Role for \"All-Authenticated in Realm\"? (Y|N)(YES|NO)').upper()
+            if auth == 'Y':
+                role_auth = 'No Yes'
+            else:
+                role_auth = 'No No'
+        role_users = raw_input('Enable Mail-User Role for single Users? (Type casesensitiv uid, empty for none, multiple uids seperate by \"|\")')
+        role_groups = raw_input('Enable Mail-User Role for a Group? (Type casesensitiv Groupname, empty for no groups, multiple Groups seperated by \"|\")')
+             
+    elif state == 'DISABLE':
+        print 'Disable socialmail'   
+        role_auth='No No'
+        role_users=''
+        role_groups=''
+        
+    apps = ['Common','WidgetContainer']    
+    for app in apps:     
+        print "Setting Role for " + app
+        setJ2EERole(app,role_auth,role_users,role_groups)
+    
+    AdminConfig.save()       
+    
+
+printMenu()
