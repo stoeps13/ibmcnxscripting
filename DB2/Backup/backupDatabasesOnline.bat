c4f0712b7c279b24681331aee121cfb5f62fa532
Christoph Stoettner <christoph.stoettner@stoeps.de>
2014-04-08 08:30:06 +0200

    Bugfixing, findstr gave empty list in prior version

--- DB2/Backup/backupDatabasesOnline.bat
+++ DB2/Backup/backupDatabasesOnline.bat
@@ -2,13 +2,15 @@
 REM
 REM Author: Klaus Bild
 REM E-Mail: klaus.bild@gmail.com
-
+REM History:    20140210   Klaus Bild           Initial Version
+                20140408   Christoph Stoettner  Fix for caseinsensitiv findstr
+                
 REM Needed for variable expansion
 SETLOCAL ENABLEDELAYEDEXPANSION
 REM Set Backup-Directory, change to your environment
-set DBBACKUPPATH=E:\Transfer
+set DBBACKUPPATH=d:\db2\backup
 REM get all databases of db2 instance and loop through the list
-FOR /F "tokens=5" %%a IN ('DB2CMD.EXE -c -w -i DB2 list DATABASE DIRECTORY ^| findstr "Alias"') DO (
+FOR /F "tokens=3 delims== " %%a IN ('DB2CMD.EXE -c -w -i DB2 list DATABASE DIRECTORY ^| findstr /i "Alias"') DO (
 				set DATABASES=%%a,!DATABASES!
 				title Starting database %%a backup on %date% at %time%... 
 				DB2CMD.EXE -c -w -i DB2 BACKUP DATABASE %%a ONLINE to %DBBACKUPPATH% COMPRESS INCLUDE LOGS
