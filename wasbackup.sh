5bef2d35b8cb77dc4ea802f0d3464404dce77901
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-12-23 09:40:42 +0100

    add zip extension

--- wasbackup.sh
+++ wasbackup.sh
@@ -4,4 +4,4 @@ export WAS_HOME=/opt/IBM/WebSphere/AppServer
 export WAS_BCK_PATH=/mnt/data/wasbackup
 
 cd $WAS_HOME/bin
-./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d` -nostop -username wasadmin -password password
+./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d`.zip -nostop -username wasadmin -password password
