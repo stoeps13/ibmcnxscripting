333ca3d74e3c13848fc1f90099ace2df1ad88068
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-12-23 09:38:53 +0100

    Backup WAS Configuration
    Shell script to backup WebSphere configuration

---
+++ wasbackup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export WAS_HOME=/opt/IBM/WebSphere/AppServer
+export WAS_BCK_PATH=/mnt/data/wasbackup
+
+cd $WAS_HOME/bin
+./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d` -nostop -username wasadmin -password password
