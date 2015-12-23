28d93fb43e1b4190b643e8a26d69f449ae536e87
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-12-23 09:56:43 +0100

    typo

--- wasbackup.sh
+++ wasbackup.sh
@@ -4,7 +4,7 @@ export WAS_HOME=/opt/IBM/WebSphere/AppServer
 export WAS_BCK_PATH=/mnt/data/wasbackup
 
 cd $WAS_HOME/bin
-if ./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d-%H%M`.zip -nostop -username wasadmin -password 4VtsIEzEA1 ; then
+if ./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d-%H%M`.zip -nostop -username wasadmin -password password ; then
          echo "Backup successfully!"
          find $WAS_BCK_PATH/* -type f -mtime +3 -exec rm {} \;
 else
