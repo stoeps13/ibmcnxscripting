8e9a76c5d2f13d33536e3e326522c1bd69be7c6d
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-12-23 09:56:26 +0100

    Added delete command for old backups

--- wasbackup.sh
+++ wasbackup.sh
@@ -4,4 +4,9 @@ export WAS_HOME=/opt/IBM/WebSphere/AppServer
 export WAS_BCK_PATH=/mnt/data/wasbackup
 
 cd $WAS_HOME/bin
-./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d`.zip -nostop -username wasadmin -password password
+if ./backupConfig.sh $WAS_BCK_PATH/websphere-config-`date +%Y%m%d-%H%M`.zip -nostop -username wasadmin -password 4VtsIEzEA1 ; then
+         echo "Backup successfully!"
+         find $WAS_BCK_PATH/* -type f -mtime +3 -exec rm {} \;
+else
+        echo "Backup failed"
+fi
