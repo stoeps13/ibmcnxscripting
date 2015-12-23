e8a3e70e4beb1c6ed05d79c8ca1f1edb2756084e
Christoph Stoettner <christoph.stoettner@stoeps.de>
2015-12-23 09:34:20 +0100

    Better readable configuration
    seperated some commands

--- DB2/Backup/configureArchiveLogs.bat
+++ DB2/Backup/configureArchiveLogs.bat
@@ -14,6 +14,11 @@ FOR /F "tokens=3 delims== " %%a IN ('DB2CMD.EXE -c -w -i DB2 list DATABASE DIREC
 				set DATABASES=%%a,!DATABASES!
 				title Starting database %%a Log configuration on %date% at %time%... 
                 REM DB2CMD.EXE -c -w -i DB2 UPDATE DATABASE CONFIGURATION FOR %%a using LOGARCHMETH1 LOGRETAIN AUTO_DEL_REC_OBJ ON num_db_backups 1 rec_his_retentn 0 logarchmeth1 disk:%DBLOGPATH%
-                DB2 UPDATE DATABASE CONFIGURATION FOR %%a using LOGARCHMETH1 LOGRETAIN AUTO_DEL_REC_OBJ ON num_db_backups 1 rec_his_retentn 0 logarchmeth1 disk:%DBLOGPATH%
+                DB2 UPDATE DATABASE CONFIGURATION FOR %%a using logarchmeth1 disk:%DBLOGPATH%
+                db2 update db cfg for %%a using AUTO_DEL_REC_OBJ ON
+ 		db2 update db cfg for %%a using num_db_backups 2
+ 		db2 update db cfg for %%a using rec_his_retentn 0
+ 		db2 update db cfg for %%a using LOGARCHCOMPR1 on
+ 		db2 update db cfg for %%a using LOGFILSIZ 8196
 				)
 ECHO Successfull configured logs for %DATABASES% in %DBLOGPATH%
