39157dc6b1f7735f9b99190982271cbe7513d43f
Christoph Stoettner <christoph.stoettner@stoeps.de>
2016-01-21 21:20:21 +0100

    DB2 Backup Scripts, script to add wasadmin to profiles

---
+++ DB2/ibmcnx/insertwasadmin.sql
@@ -0,0 +1,4 @@
+connect to peopledb;
+insert into  empinst.employee VALUES('00000000-0000-0000-0000-000000000000','wasadmin','wasadmin',CURRENT TIMESTAMP,'','','91f7bf55-12f6-421b-b53a-8c2cb0e8ad9b','uid=wasadmin,o=defaultWIMFileBasedRealm','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','uid=wasadmin,o=defaultwimfilebasedrealm','00000000-0000-0000-0000-040508202233',0,0);
+connect reset;
+
